4ddec0c4b9395b3564ee4c7113a8de2f46dc5774
Nameson Gaudel <namesongaudel.ng@gmail.com>
2024-04-14 10:57:57 -0400

    feat: initial setup with fast api

---
+++ app/main.py
@@ -0,0 +1,28 @@
+"""
+Main Application File for our FastApi Server
+"""
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://www.google.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+@ app.get("/")
+async def root():
+    return {"message": "This is the main page"}
